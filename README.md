e6bb6bf69561cc52cef92d36feb44e5663fcf9b6
Fabien Castan <fabien.castan@free.fr>
2016-05-07 16:46:09 +0200

    Template plugin with generator script

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# pluginOFX
-OpenFX plugin example, buit on Travis CI.
+PLUGIN_LABEL
+============
 
-[![Build Status](https://travis-ci.org/tuttleofx/pluginOFX.svg?branch=master)](https://travis-ci.org/tuttleofx/pluginOFX) 
+PLUGIN_DESCRIPTION
+
+[PLUGIN_LABEL on ShuttleOFX.](http://shuttleofx.org/plugin/PLUGIN_UID)
+
+## Compilation
+
+```
+git submodule update -i
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=../install
+make install
+```
---
+++ createPlugin.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import json
+
+
+def replaceString(inputStr, params):
+    '''
+    '''
+    outputStr = str(inputStr)
+    for key, value in params.iteritems():
+        outputStr = outputStr.replace(key.upper(), value)
+    return outputStr
+
+
+def replaceInFile(filepath, params):
+    '''
+    '''
+    with open(filepath, 'r') as dataFileIn:
+        inputStr = dataFileIn.read()
+    outputStr = replaceString(inputStr, params)
+    if inputStr != outputStr:
+        with open(filepath, 'w') as dataFileOut:
+            dataFileOut.write(outputStr)
+
+
+def renameFile(filepath, params):
+    '''
+    '''
+    folder, filename = os.path.split(filepath)
+    newFilename = replaceString(filename, params)
+    newFilepath = os.path.join(folder, newFilename)
+    if filename != newFilename:
+        os.rename(filepath, newFilepath)
+    return newFilepath
+
+
+if  __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Rename the template files to create a new OpenFX Plugin.')
+    parser.add_argument('params', help='JSON files with parameters')
+    parser.add_argument('path', help='Path to the OFX template')
+    args = parser.parse_args()
+
+    with open(args.params, 'r') as paramsFile:
+        params = json.load(paramsFile)
+
+    # print "args.params:", args.params
+    # print "args.path:", args.path
+    for root, folders, filenames in os.walk(args.path):
+        for filename in filenames:
+            filepath = os.path.join(root, filename)
+            newFilepath = renameFile(filepath, params)
+            replaceInFile(newFilepath, params)
